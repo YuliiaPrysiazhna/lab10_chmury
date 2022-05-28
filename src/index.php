6cb49d2b6e7c7903375ba621f5268e1251619f38
YuliiaPrysiazhna <103123474+YuliiaPrysiazhna@users.noreply.github.com>
2022-05-28 18:35:44 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Laboratorium 9 Dmytro Shyamsnkyi</title>
+        <title>Laboratorium 10</title>
         <style>
             body {
                 font-family: "Arial", sans-serif;
